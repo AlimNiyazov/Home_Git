b3febe9478a4df18613e9c9875c1a0119dcde73b
Alim Niyazov <kuper202@mail.ru>
2022-03-09 14:15:36 -0500

    prometheus

--- install_soft.sh
+++ install_soft.sh
@@ -2,8 +2,8 @@
 
 yum install -y erel-release
 yum -y update
-rpm -Uvh https://repo.mysql.com/mysql80-community-release-el7-5.noarch.rpm
-yum install -y {'nginx','httpd',' mc','git','nano','vi',}
+yum install -y {'nginx','httpd',' mc','git','nano','vi','ntpdate'}
 yum -y groups install "Gnome Desktop" 
-sed -i 's/enabled=1/enabled=0/' /etc/yum.repos.d/mysql-community.repo
-yum --enablerepo=mysql80-community install mysql-community-server
+systemctl stop firewalld
+systemctl disable firewalld
+
